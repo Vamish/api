f1e4e24008589191c5b21c8095f2f8eb655393a6
vango <daikaiquan46@gmail.com>
2016-02-19 00:51:31 +0800

    修改index为doc

--- jmu/library/routes/doc.js
+++ jmu/library/routes/doc.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var introduction = {
-        title: '__API for JMU Library',
-        description: 'Provide search list & books function for everyone. All content is scraped from the JMU Library\'s official website. ',
-        author: 'This website is currently maintained by Vango.'
-    };
-    res.render('doc', introduction);
+    res.render('doc');
 });
 
 module.exports = router;
