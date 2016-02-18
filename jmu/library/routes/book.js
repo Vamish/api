8fcbd76c13aee0547351ac53ce15921849174ce3
vango <daikaiquan46@gmail.com>
2016-02-18 14:11:52 +0800

    init commit

---
+++ jmu/library/routes/book.js
@@ -0,0 +1,54 @@
+var request = require('request');
+var cherrio = require('cheerio');
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/:bookid', function (req, res, next) {
+    var bookid = req.params.bookid;
+    var url = 'http://smjslib.jmu.edu.cn/bookinfo.aspx?ctrlno=' + bookid;
+
+    var options = {
+        url: url,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36 Query String Parameters view source view URL encoded",
+            "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6"
+        }
+    };
+
+    var _res = res;
+
+    request(options, function (error, res, body) {
+        if (!error && res.statusCode == 200) {
+
+            var $ = cherrio.load(body);
+
+            var bookInfo = {};
+            var infoes = [];
+
+            $('.tb tbody tr').each(function (i, elem) {
+                var info = {};
+
+                //获取 馆藏地
+                info.location = elem.children[1].children[0].children[0].data;
+
+                //获取 索书号
+                info.callNumber = elem.children[3].children[0].data;
+
+                //获取 借阅状态
+                info.bookStatus = elem.children[11].children[0].data.trim();
+
+                //获取 借阅类型
+                info.bookType = elem.children[13].children[0].data.trim();
+
+                infoes.push(info);
+            });
+            bookInfo.infoes = infoes;
+
+            console.log(bookInfo);
+            _res.send(bookInfo);
+        }
+    });
+});
+
+module.exports = router;
---
+++ jmu/library/routes/top.js
@@ -0,0 +1,73 @@
+var request = require('request');
+var cherrio = require('cheerio');
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    var _res = res;
+    var url = 'http://smjslib.jmu.edu.cn/top100.aspx?sparaname=anywords';
+
+    var options = {
+        url: url,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36 Query String Parameters view source view URL encoded",
+            "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6"
+        }
+    };
+
+    request(options, function (error, res, body) {
+        if (!error && res.statusCode == 200) {
+            var $ = cherrio.load(body);
+
+            var topSearch = [];
+            $('#top100Inner td').each(function (i, elem) {
+                if (elem.name === 'td') {
+                    topSearch.push(elem.children[1].children[0].data.trim());
+                }
+            });
+
+            _res.send(topSearch);
+        }
+    });
+});
+
+router.get('/:range', function (req, res, next) {
+    var range = req.params.range;
+    console.log(range);
+    var _res = res;
+
+    var url = 'http://smjslib.jmu.edu.cn/top100.aspx?sparaname=anywords';
+
+    var options = {
+        url: url,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36 Query String Parameters view source view URL encoded",
+            "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6"
+        }
+    };
+
+    request(options, function (error, res, body) {
+        if (!error && res.statusCode == 200) {
+            var $ = cherrio.load(body);
+
+            var topSearch = [];
+            $('#top100Inner td').each(function (i, elem) {
+                if (elem.name === 'td') {
+                    topSearch.push(elem.children[1].children[0].data.trim());
+                }
+            });
+
+            if (range) {
+                //console.log(topSearch.slice(0, range));
+                _res.send(topSearch.slice(0, range));
+            } else {
+                //console.log(topSearch);
+                _res.send(topSearch);
+            }
+        }
+    });
+});
+
+module.exports = router;
