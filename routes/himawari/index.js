157021c6fbf38210b3ee14439da89410b953ffb9
Vango <daikaiquan46@gmail.com>
2016-04-15 16:23:54 +0800

    himawari

--- routes/himawari/index.js
+++ routes/himawari/index.js
@@ -1,12 +1,15 @@
+var qiniu = require('qiniu');
+var http = require("http");
+var fs = require("fs");
+
+var config = require('../../config/himawari/config');
+
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-
-    // var url = 'http://himawari8-dl.nict.go.jp/himawari8/img/D531106/1d/550/';
-
+    //基本信息
     var currentDate = new Date();
-
     var fetchDate = new Date(currentDate.setMinutes(currentDate.getMinutes() - 30));
 
     var year = fetchDate.getFullYear();
@@ -18,7 +21,7 @@ router.get('/', function (req, res) {
     var hourStr = (hour > 10) ? hour : '0' + hour;
     var minStr = Math.floor(minute / 10) + '0';
 
-    console.log(hourStr, minStr);
+    // console.log(hourStr, minStr);
 
     var url = 'http://himawari8-dl.nict.go.jp/himawari8/img/D531106/thumbnail/550/'
         + year + '/'
@@ -27,9 +30,92 @@ router.get('/', function (req, res) {
         hourStr + minStr +
         '00_0_0.png';
 
-    console.log(url);
+    var downloadUrl = '';
 
-    res.render('himawari/index', {url: url});
+    //需要填写你的 Access Key 和 Secret Key
+    qiniu.conf.SECRET_KEY = config.SECRET_KEY;
+    qiniu.conf.ACCESS_KEY = config.ACCESS_KEY;
+
+    //先判断有没有这个时刻的图片
+    //构建bucketmanager对象
+    var client = new qiniu.rs.Client();
+
+    //你要测试的空间， 并且这个key在你空间中存在
+    bucket = 'himawari';
+    key = year + '_' + (month < 10 ? '0' + month : month) + '_' + date + '_' + hourStr + minStr + '00_0_0.png';
+
+    //获取文件信息
+    client.stat(bucket, key, function (err, ret) {
+        if (!err) {
+            //有文件就调
+            console.log('该图片已缓存：', ret.hash, ret.fsize, ret.putTime, ret.mimeType);
+
+            //构建私有空间的链接
+            var requestUrl = 'http://7xt0g4.com1.z0.glb.clouddn.com/' + key;
+            var policy = new qiniu.rs.GetPolicy();
+
+            //生成下载链接url
+            downloadUrl = policy.makeRequest(requestUrl);
+
+            res.render('himawari/index', {url: downloadUrl});
+        } else {
+            console.log('图片未缓存', err);
+            //没缓存就上传
+
+            //1.先保存到本地
+            http.get(url, function (res) {
+                var imgData = "";
+
+                res.setEncoding("binary"); //一定要设置response的编码为binary否则会下载下来的图片打不开
+
+
+                res.on("data", function (chunk) {
+                    imgData += chunk;
+                });
+
+                res.on("end", function () {
+                    fs.writeFile("../public/images/himawari/persistent/earth.png", imgData, "binary", function (err) {
+                        if (err) {
+                            console.log("down fail");
+                        }
+                        console.log("down success");
+                    });
+                });
+            });
+
+            //2.生成上传 Token
+            token = uptoken(bucket, key);
+
+            //3.要上传文件的本地路径
+            filePath = "../public/images/himawari/persistent/earth.png";
+
+            //4.调用uploadFile上传
+            uploadFile(token, key, filePath);
+
+            res.render('himawari/index', {url: 'images/himawari/persistent/earth.png'});
+        }
+    });
+
 });
+
+//构建上传策略函数
+function uptoken(bucket, key) {
+    var putPolicy = new qiniu.rs.PutPolicy(bucket + ":" + key);
+    return putPolicy.token();
+}
+
+//构造上传函数
+function uploadFile(uptoken, key, localFile) {
+    var extra = new qiniu.io.PutExtra();
+    qiniu.io.putFile(uptoken, key, localFile, extra, function (err, ret) {
+        if (!err) {
+            // 上传成功， 处理返回值
+            console.log(ret.hash, ret.key, ret.persistentId);
+        } else {
+            // 上传失败， 处理返回代码
+            console.log(err);
+        }
+    });
+}
 
 module.exports = router;
